dfb02276af4ff5a1cdd38ac457509632dac46fa3
Mihail Curmei <mcurmei@mail.ru>
2021-12-14 23:14:57 +0200

    Implemented file manager

--- p3passwordmanager/src/main/java/PasswordManager/Application/FileManager.java
+++ p3passwordmanager/src/main/java/PasswordManager/Application/FileManager.java
@@ -1,28 +1,110 @@
 package PasswordManager.Application;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.List;
 
-import PasswordManager.Generators.PasswordEntry;
+import PasswordManager.Generators.DataEntry;
 
 public class FileManager {
-    private static String passLibraryPath = "";
-    private static String passLibraryBackupPath = "";
+    private static String passLibraryPath = "passfileset.pmg";
+    private static String passLibraryBackupPath = "passfileset.pmg.bak";
 
-    private static ArrayList<PasswordEntry> passLibrary;
+    private static List<DataEntry> passLibrary;
 
     public static void openLibrary() {
-
+        try {
+            FileInputStream fis = new FileInputStream(passLibraryPath);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            passLibrary = (ArrayList)ois.readObject();
+            
+            ois.close();
+            fis.close();
+        } catch (FileNotFoundException e) {
+            createLibraryFile();
+        } catch (ClassNotFoundException e) {
+            restoreBackup();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void restoreBackup() {
-        
+        try {
+            //File passLibraryFile = new File(passLibraryPath);
+            FileInputStream fis = new FileInputStream(passLibraryBackupPath);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            passLibrary = (ArrayList)ois.readObject();
+            ois.close();
+            fis.close();
+
+            saveLibrary(); //called in case original file was damaged or lost
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createLibraryFile() {
+        try {
+            File passLibraryFile = new File(passLibraryPath);
+            passLibraryFile.createNewFile();
+
+            File passLibraryBackupFile = new File(passLibraryBackupPath);
+            if (passLibraryBackupFile.exists()) {
+                restoreBackup();
+                return;
+            } else {
+                passLibrary = new ArrayList<>();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            //System.out.print(e);
+        }
+    }
+
+    private static void createLibraryBackupFile() {
+        try {
+            File passLibraryBackupFile = new File(passLibraryBackupPath);
+            passLibraryBackupFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            //System.out.print(e);
+        }
     }
 
     public static void saveLibrary() {
-        
+        try {
+            FileOutputStream fos = new FileOutputStream(passLibraryPath);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(passLibrary);
+            oos.close();
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 
     public static void closeLibrary() {
-        
+        saveLibrary();
+
+        try {
+            FileOutputStream fos = new FileOutputStream(passLibraryBackupPath);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(passLibrary);
+            oos.close();
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public static List<DataEntry> getLibrary() {
+        return passLibrary;
     }
 }
