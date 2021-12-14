fe6ff43381c924c5f8ae978cdbe97d5f9a9dc8eb
Mihail Curmei <mcurmei@mail.ru>
2021-12-14 20:33:47 +0200

    File manager mockup added

---
+++ p3passwordmanager/src/main/java/PasswordManager/Application/FileManager.java
@@ -0,0 +1,28 @@
+package PasswordManager.Application;
+
+import java.util.ArrayList;
+
+import PasswordManager.Generators.PasswordEntry;
+
+public class FileManager {
+    private static String passLibraryPath = "";
+    private static String passLibraryBackupPath = "";
+
+    private static ArrayList<PasswordEntry> passLibrary;
+
+    public static void openLibrary() {
+
+    }
+
+    public static void restoreBackup() {
+        
+    }
+
+    public static void saveLibrary() {
+        
+    }
+
+    public static void closeLibrary() {
+        
+    }
+}
