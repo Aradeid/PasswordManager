dc310cc39e4f3e29f415fe37cfbd0f5ba8e1b9f1
Mihail Curmei <mcurmei@mail.ru>
2022-01-25 01:35:35 +0200

    Made settings a static value

--- p3passwordmanager/src/main/java/PasswordManager/Application/Settings.java
+++ p3passwordmanager/src/main/java/PasswordManager/Application/Settings.java
@@ -1,68 +1,11 @@
 package PasswordManager.Application;
 
 public class Settings {
-    private int windowHeight;
-    private int windowWidth;
-    private boolean databaseEnabled;
-    private String libraryFilePath;
-    private String backupLibraryFilePath;
-    public Settings() {
-        //TODO load settings from file
-
-        windowHeight = 400;
-        windowWidth = 600;
-        databaseEnabled = false;
-        libraryFilePath = "passfileset.pmg";
-        backupLibraryFilePath = "passfileset.pmg.bak";
-    }
-
-    private void updateSettingsFile() {
-        //TODO implement file handling
-    }
-
-    public int getWindowHeight() {
-        return windowHeight;
-    }
-
-    public void setWindowHeight(int height) {
-        //TODO save to file
-        windowHeight = height;
-        updateSettingsFile();
-    }
-
-    public int getWindowWidth() {
-        return windowWidth;
-    }
-
-    public void setWindowWidth(int width) {
-        //TODO save to file
-        windowWidth = width;
-        updateSettingsFile();
-    }
-
-    public boolean getDatabaseEnabled() {
-        return databaseEnabled;
-    }
-
-    public void setDatabaseEnabled(boolean value) {
-        //TODO save to file
-        databaseEnabled = value;
-        updateSettingsFile();
-    }
-
-    public String getLibraryFilePath() {
-        return libraryFilePath;
-    }
-
-    public void setLibraryFilePath(String path) {
-        this.libraryFilePath = path;
-    }
-
-    public String getBackupLibraryFilePath() {
-        return backupLibraryFilePath;
-    }
-
-    public void setBackupLibraryFilePath(String path) {
-        this.backupLibraryFilePath = path;
-    }
+    final static public int WindowHeight = 400;
+    final static public int WindowWidth = 600;
+    final static public boolean DatabaseEnabled = false;
+    final static public String LibraryFilePath = "passfileset.pmg";
+    final static public String BackupLibraryFilePath = "passfileset.pmg.bak";
+    final static public String DatabaseUrl = "jdbc:mysql://localhost:passwordmanager";
+    final static public String DatabaseUser = "root";
 }
