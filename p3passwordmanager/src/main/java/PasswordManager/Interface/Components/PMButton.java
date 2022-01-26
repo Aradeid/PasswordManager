344fd338c653a6e7d803117372ff456f22a56a0c
Mihail Curmei <mcurmei@mail.ru>
2022-01-26 23:04:13 +0200

    More UI components, missed tracking on previous commit

---
+++ p3passwordmanager/src/main/java/PasswordManager/Interface/Components/PMButton.java
@@ -0,0 +1,10 @@
+package PasswordManager.Interface.Components;
+
+import javax.swing.JButton;
+
+public class PMButton extends JButton {
+    public PMButton(String name) {
+        super(name);
+        this.setSize(100,30);
+    }
+}
